91f0df35f7f736e8d26db7577268b43028f7f6fb
Sele3 <STAN266@e.ntu.edu.sg>
2023-04-22 12:27:44 +0800

    wip: logincontroller class
    handles the user login and redirects them to the appropriate boundary after login

---
+++ Controller/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using FYPManager.Boundary.Services;
+using FYPManager.Entity;
+using FYPManager.Entity.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYPManager.Controller;
+
+public class LoginController
+{
+    private readonly FYPMContext _context;
+
+    public LoginController(FYPMContext context)
+    {
+        _context = context;
+    }
+
+    public void LoginAs<T>(string username, string password) where T : User
+    {
+        //var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        //if (user != null)
+        //{
+        //    return true;
+        //}
+    }
+}
---
+++ Controller/Utility/UserBoundaryFactory.cs
@@ -0,0 +1,13 @@
+﻿using FYPManager.Entity.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYPManager.Controller.Utility;
+
+public class UserBoundaryFactory
+{
+    
+}
