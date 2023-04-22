18e8b9e6eb2a78e1d6df70d2de4c5329d620ff5a
Sele3 <STAN266@e.ntu.edu.sg>
2023-04-22 12:28:49 +0800

    feat: appsettings + connectionstring
    add appsettings file to contain the defaultconnection instead

--- Entity/FYPMContext.cs
+++ Entity/FYPMContext.cs
@@ -5,11 +5,9 @@ namespace FYPManager.Entity;
 
 public class FYPMContext : DbContext
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public FYPMContext(DbContextOptions<FYPMContext> options) : base(options)
     {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FYPManager;Trusted_Connection=True;");
     }
-
 
     public DbSet<Student> Students { get; set; }
     public DbSet<Supervisor> Supervisors { get; set; }
