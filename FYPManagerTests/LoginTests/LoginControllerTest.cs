36a21c34c019347cd211f35bd65b6352ee8fa2e1
Sele3 <STAN266@e.ntu.edu.sg>
2023-04-28 10:55:06 +0800

    test: logincontroller + userboundaryfactory

---
+++ FYPManagerTests/LoginTests/LoginControllerTest.cs
@@ -0,0 +1,90 @@
+﻿using FYPManager.Boundary.Services;
+using FYPManager.Boundary.UserBoundary;
+using FYPManager.Controller;
+using FYPManager.Entity.Users;
+using FYPManager.Exceptions;
+using Moq;
+
+namespace FYPManagerTests.LoginTests;
+
+[TestClass]
+public class LoginControllerTest : BaseTest
+{
+    private readonly Mock<SupervisorBoundary> _mockUserBoundary = new();
+    private LoginController _loginController = null!;
+
+    [TestInitialize]
+    public override void Setup()
+    {
+        base.Setup();
+
+        var _mockServiceProvider = new Mock<IServiceProvider>();
+        _mockServiceProvider
+            .Setup(x => x.GetService(typeof(SupervisorBoundary)))
+            .Returns(_mockUserBoundary.Object);
+
+        _loginController = new LoginController(_context, _mockServiceProvider.Object);
+    }
+
+    [TestMethod]
+    public void TestSuccessfulSupervisorLogin()
+    {
+        // Arrange
+        var userID = "asfli";
+        var password = HashService.Hash();
+
+        // Act
+        PerformLogin<Supervisor>(userID, password);
+
+        // Assert
+        _mockUserBoundary.Verify(b => b.Run(), Times.Once);
+        Assert.AreEqual(userID, UserSession.GetCurrentUser().UserID);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(LoginException))]
+    public void TestWrongUserIDSupervisorLogin()
+    {
+        // Arrange
+        var userID = "unknown";
+        var password = HashService.Hash();
+        // Act
+        PerformLogin<Supervisor>(userID, password);
+
+        // Assert
+        // Expects LoginException to be thrown
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(LoginException))]
+    public void TestWrongPasswordSupervisorLogin()
+    {
+        // Arrange
+        var userID = "asfli";
+        var password = "wrong";
+
+        // Act
+        PerformLogin<Supervisor>(userID, password);
+
+        // Assert
+        // Expects LoginException to be thrown
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(LoginException))]
+    public void TestStudentLoginWithSupervisorCredentials()
+    {
+        // Arrange
+        var userID = "asfli";
+        var password = HashService.Hash();
+
+        // Act
+        PerformLogin<Student>(userID, password);
+
+        // Assert
+        // Expects LoginException to be thrown
+    }
+
+    private void PerformLogin<T>(string userID, string password) where T : User
+        => _loginController.LoginAs<T>(userID, password);
+}
---
+++ FYPManagerTests/LoginTests/UserBoundaryFactoryTest.cs
@@ -0,0 +1,48 @@
+﻿using FYPManager.Boundary.UserBoundary;
+using FYPManager.Controller.Utility;
+using FYPManager.Entity.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FYPManagerTests.LoginTests;
+
+[TestClass]
+public class UserBoundaryFactoryTest
+{
+    private IServiceProvider _serviceProvider = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var services = new ServiceCollection();
+        services.AddScoped<StudentBoundary>();
+        services.AddScoped<SupervisorBoundary>();
+        services.AddScoped<CoordinatorBoundary>();
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    [TestMethod]
+    public void TestGetCorrectUserBoundary()
+    {
+        // Arrange
+        var expectedBoundary = _serviceProvider.GetService<CoordinatorBoundary>();
+
+        // Act
+        var actualBoundary = UserBoundaryFactory.GetUserBoundary<Coordinator>(_serviceProvider);
+
+        // Assert
+        Assert.AreEqual(expectedBoundary!.GetType(), actualBoundary.GetType());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TestGetInvalidUserBoundary()
+    {
+        // Arrange
+
+        // Act
+        UserBoundaryFactory.GetUserBoundary<User>(_serviceProvider);
+
+        // Assert
+        // Expects ArgumentException to be thrown
+    }
+}
